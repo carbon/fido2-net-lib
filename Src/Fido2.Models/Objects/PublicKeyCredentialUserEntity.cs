dff96f49d980528c64c257bd052c7c5104424ec6
Jason Nelson <jason@carbonmade.com>
2024-11-11 20:24:25 -0800

    Matched requireness of properties to specification

--- Src/Fido2.Models/Objects/PublicKeyCredentialUserEntity.cs
+++ Src/Fido2.Models/Objects/PublicKeyCredentialUserEntity.cs
@@ -6,12 +6,12 @@ public sealed class PublicKeyCredentialUserEntity
 {
 #pragma warning disable IL2026 // Members annotated with 'RequiresUnreferencedCodeAttribute' require dynamic access otherwise can break functionality when trimming application code
     [MaxLength(64)]
-    public byte[] Id { get; set; }
+    public required byte[] Id { get; set; }
 #pragma warning restore IL2026 // Members annotated with 'RequiresUnreferencedCodeAttribute' require dynamic access otherwise can break functionality when trimming application code
 
     public string Name { get; set; }
 
-    public string DisplayName { get; set; }
+    public required string DisplayName { get; set; }
 
     public string Icon { get; set; }
 }
